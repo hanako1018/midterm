3b8cbf061060823cc82f18d96b29750cbde80d4f
hanako1018 <hanakokato.harry@gmail.com>
2014-11-13 20:22:26 +0900

    0

--- app/09/factorial.js
+++ app/09/factorial.js
@@ -1,9 +1,25 @@
 
 var factorial = function(a){
 
+var counter = a;
+if(counter <= 0){
 
-if(Number.isInteger(a)== true){
+return null;
 
-return  null;
 };
+
+while(counter > 0);
+var a = Math.floor(a);
+
+if(Number.isInteger(a) && a > 0){
+
+
+for(var i = a - 1;i>0;i-){
+  a = a*i;
+
+}
+return a;
+
+
+
 };
--- app/10/prime.js
+++ app/10/prime.js
@@ -5,6 +5,8 @@ var prime = function(a){
 
 if(Number.isInteger(a)== false){
 
-return  false;
+return  a*(a-1);
+
+false;
 };
 };
